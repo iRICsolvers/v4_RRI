fe5b65a5df0cd6876bd5f5e441fe4f7b3841558c
starfieldsKt <keita.hoshino.1001@gmail.com>
2022-11-22 17:10:59 +0900

    辞書ファイル追加

---
+++ rri_v1/translation_ja_JP.ts
@@ -0,0 +1,400 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="ja_JP">
+    <!--これは iRIC で使用する辞書ファイルです。
+<translation> と </translation> の間に、翻訳後の文字列を追記してください。
+-->
+    <context>
+        <message>
+            <source>Rainfall-Runoff-Inundation v1.4.2.2 64 bit</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Base Conditions</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>RRI_Input_Format_Ver1_4_2 for iRIC</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Run Type</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Make Geographic Condition Only</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Run Only</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Geographic Data file</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Dem File</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Acc File</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Dir File</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>River Shape Parameters</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Width : Cw, Sw</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>width_param_s</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Depth : Cd, Sd</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>depth_param_c</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>depth_param_s</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Height : Height, Limit</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>height_param</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>height_limit_param</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Rain</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Rain File</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>xllcorner_rain</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>yllcorner_rain</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>cellsize_rain_x</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>cellsize_rain_y</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Time Control</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Simulation Time[hour]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Time Step for Slope Simulation[sec]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Time Step for River Simulation[sec]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Number for Output Times(without Initial)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>River Simulation Parameters</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Manning Roughness for River Region</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>threshold of flow accumulation</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Slope Simulation Parameters</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Green-Ampt infiltration model parameters</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Region 1</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Region 2</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Region 3</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Region 4</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Region 5</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source></source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Diff or Kinematic</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Manning Roughness for Slope</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>soildepth[m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Porosity [-]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Kinematic</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Diffusion</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>ksv[m/s]                   </source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Sf[m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>lateral subsurface and surface model parameters</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>ka[m/s]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Unsat. porosity [-]        </source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>beta</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Unsat. porosity [-]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Initial Condition for Hotstart</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Settings</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>initfile_slo</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>No</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Yes</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>initfile_riv</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>initfile_gw</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>initfile_gampt_ff</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Output asc File format</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_hs</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_hr</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_hg</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_qr</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_qu</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_qv</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_gu</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_gv</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_gampt_ff</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>outfile_storage</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Elevation [m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>DIR</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>ACC</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Width[m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Depth[m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Height[m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Land Use Type</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Rain[mm/h]: Data Check Only</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Time</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Dam</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Name</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Storage Volume[m3]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Constant Discharge[m3/s]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Time Series for River Discharge at point</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>qr[m3/s]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Time Series for River Depth at point</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>hr[m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Time Series for Slope Discharge at point</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>qs[m3/s]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Time Series for Slope Depth at point</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>hs[m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>DivSet</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>div_dest_i</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>div_dest_j</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>div_rate</source>
+            <translation></translation>
+        </message>
+    </context>
+</TS>
