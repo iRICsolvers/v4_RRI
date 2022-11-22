42233b281e80620af527e14f58213da31731b7e7
starfieldsKt <keita.hoshino.1001@gmail.com>
2022-11-22 15:14:33 +0900

    make.bat更新

--- rri_v1/make_check_cgns.bat
+++ rri_v1/make_check_cgns.bat
@@ -1,8 +1,10 @@
 call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2019
 
 del *.obj *.mod
-ifort /c /O3 ../check_cgns/check_cgns.f90
-ifort /O3 *.obj ../lib/iriclib.lib /o  ./check_cgns.exe
+
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/iric.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../check_cgns/check_cgns.f90
+ifort /MD /Qopenmp /O3 *.obj ../lib/iriclib.lib /o  ./check_cgns.exe
 
 
 del *.obj 
--- rri_v1/make_demAdjust2.bat
+++ rri_v1/make_demAdjust2.bat
@@ -1,9 +1,10 @@
 call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2019
 
 del *.obj *.mod
-ifort /c /O3 ../demAdjust2/RRI_IRIC_Mod2.f90
-ifort /c /O3 ../demAdjust2/demAdjust2.f90
-ifort /O3 *.obj ../lib/iriclib.lib /o  ./demAdjust2.exe
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/iric.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../demAdjust2/RRI_IRIC_Mod2.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../demAdjust2/demAdjust2.f90
+ifort /MD /Qopenmp /O3 *.obj ../lib/iriclib.lib /o  ./demAdjust2.exe
 editbin /stack:40000000 ./demAdjust2.exe
 
 del *.obj 
