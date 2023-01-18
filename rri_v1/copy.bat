14d565648d71cbd10a87314084b2eda35da3e08c
starfieldsKt <keita.hoshino.1001@gmail.com>
2023-01-18 10:28:59 +0900

    フォルダ構成修正

--- rri_v1/copy.bat
+++
@@ -1,4 +0,0 @@
-cp -f definition.xml 		"D:\ProgramFiles\iRIC_dev\solvers\rri_v1"
-cp -f rri.exe 		"D:\ProgramFiles\iRIC_dev\solvers\rri_v1"
-cp -f go.bat 		"D:\ProgramFiles\iRIC_dev\solvers\rri_v1"
-cp -f demAdjust2.exe 		"D:\ProgramFiles\iRIC_dev\solvers\rri_v1"
