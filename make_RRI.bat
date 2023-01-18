14269ab37ff74963704e6a16f713aa6f532b9901
starfieldsKt <keita.hoshino.1001@gmail.com>
2023-01-18 10:42:19 +0900

    フォルダ構成変更に合わせてパス修正

--- make_RRI.bat
+++ make_RRI.bat
@@ -1,31 +1,31 @@
 call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2022
 
 del *.obj *.mod
-ifort /MD /c /Qopenmp /O3 /nostandard-realloc-lhs ../1.4.2.3/iric.f90
-ifort /MD /c /Qopenmp /O3 /nostandard-realloc-lhs ../1.4.2.3/RRI_Mod.f90
-ifort /MD /c /Qopenmp /O3  /nostandard-realloc-lhs ../1.4.2.3/RRI_Mod2.f90
-ifort /MD /c /Qopenmp /O3  /nostandard-realloc-lhs ../1.4.2.3/RRI_Mod_Dam.f90
-ifort /MD /c /Qopenmp /O3  /nostandard-realloc-lhs ../1.4.2.3/RRI_Mod_Tecout.f90
-ifort /MD /c /Qopenmp /O3  /nostandard-realloc-lhs ../1.4.2.3/RRI_IRIC_Mod.f90
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI.f90 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Bound.f90
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Dam.f90
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Div.f90
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_DT_Check.f90
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Evp.f90 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_GW.f90
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Infilt.f90 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Read.f90
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Riv.f90 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_RivSlo.f90 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_section.f90 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Slope.f90 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Sub.f90 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_Tecout.f90 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/RRI_TSAS.f90
+ifort /MD /c /Qopenmp /O3 /nostandard-realloc-lhs ./src/RRI/iric.f90
+ifort /MD /c /Qopenmp /O3 /nostandard-realloc-lhs ./src/RRI/RRI_Mod.f90
+ifort /MD /c /Qopenmp /O3  /nostandard-realloc-lhs ./src/RRI/RRI_Mod2.f90
+ifort /MD /c /Qopenmp /O3  /nostandard-realloc-lhs ./src/RRI/RRI_Mod_Dam.f90
+ifort /MD /c /Qopenmp /O3  /nostandard-realloc-lhs ./src/RRI/RRI_Mod_Tecout.f90
+ifort /MD /c /Qopenmp /O3  /nostandard-realloc-lhs ./src/RRI/RRI_IRIC_Mod.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI.f90 
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Bound.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Dam.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Div.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_DT_Check.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Evp.f90 
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_GW.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Infilt.f90 
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Read.f90
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Riv.f90 
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_RivSlo.f90 
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_section.f90 
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Slope.f90 
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Sub.f90 
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_Tecout.f90 
+ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ./src/RRI/RRI_TSAS.f90
 
-ifort /MD /Qopenmp /O3 *.obj ../lib/iriclib.lib /o  ./rri.exe
-editbin /stack:40000000  ./rri.exe
+ifort /MD /Qopenmp /O3 *.obj ./lib/iriclib.lib /o  ./install/rri.exe
+editbin /stack:40000000  ./install/rri.exe
 
 del *.obj 
 del *.mod 
--- make_check_cgns.bat
+++ make_check_cgns.bat
@@ -2,10 +2,9 @@ call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2019
 
 del *.obj *.mod
 
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../1.4.2.3/iric.f90
-ifort /MD /c  /Qopenmp /O3   /nostandard-realloc-lhs ../check_cgns/check_cgns.f90
-ifort /MD /Qopenmp /O3 *.obj ../lib/iriclib.lib /o  ./check_cgns.exe
-
+ifort /MD /c  /Qopenmp /O3 /nostandard-realloc-lhs ./src/RRI/iric.f90
+ifort /MD /c  /Qopenmp /O3 /nostandard-realloc-lhs ./src/check_cgns/check_cgns.f90
+ifort /MD /Qopenmp /O3 *.obj ./lib/iriclib.lib /o ./install/check_cgns.exe
 
 del *.obj 
 del *.mod 
